826539b7996e18087a9a54f1bebf82502fc21dfb
ahmed-deftoner <70218377+ahmed-deftoner@users.noreply.github.com>
2022-05-18 19:37:31 +0500

    structs

--- main.go
+++ main.go
@@ -7,6 +7,35 @@ import (
 	"github.com/gorilla/mux"
 )
 
+type Block struct {
+	Pos       int
+	Data      AlbumCheckout
+	Timestamp string
+	Hash      string
+	PrevHash  string
+}
+
+type Blockchain struct {
+	blocks []*Block
+}
+
+type AlbumCheckout struct {
+	AlbumID      string `json:"album_id"`
+	User         string `json:"user"`
+	CheckoutDate string `json:"checkout_date"`
+	IsGenesis    bool   `json:"is_genesis"`
+}
+
+type Album struct {
+	ID          string `json:"id"`
+	Name        string `json:"name"`
+	Artist      string `json:"artist"`
+	ReleaseDate string `json:"release_date"`
+	Genre       string `json:"genre"`
+}
+
+var Blockchain *Blockchain
+
 func main() {
 	r := mux.NewRouter()
 	r.HandleFunc("/", getBlockchain).Methods("GET")
