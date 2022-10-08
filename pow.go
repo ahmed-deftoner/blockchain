c002ac4a0fad0a71d8eecf4d865702402596b2c0
ahmed-deftoner <70218377+ahmed-deftoner@users.noreply.github.com>
2022-10-08 16:06:27 +0500

    block

--- pow.go
+++ pow.go
@@ -7,18 +7,27 @@ import (
 	"github.com/joho/godotenv"
 )
 
+const difficulty = 1
+
 type Block struct {
+	Index      int
+	Timestamp  string
+	Data       string
+	Hash       string
+	Prevhash   string
+	Nonce      string
+	Difficulty int
 }
 
 var blockchain []Block
 
 func run() http.Handler {
+
+}
+
+func main() {
 	err := godotenv.Load()
 	if err != nil {
 		log.Fatal(err)
 	}
-}
-
-func main() {
-
 }
